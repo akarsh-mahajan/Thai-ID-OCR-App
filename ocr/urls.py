be4c34349d4e2260bb8de734a48d1fa8b4f0100d
akarsh-mahajan <akarshmahajan23@gmail.com>
2023-12-25 14:24:31 +0530

    added functionality of loading previous executions and also added the template for the same

--- ocr/urls.py
+++ ocr/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_id_card, name='upload_id_card'),
+    path('records/', views.load_previous_executions, name='load_previous_executions'),
 ]
